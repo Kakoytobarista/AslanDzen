594a26ccd802f6ddfea595e06cbce3a5723009b4
aslan <baristan1aslan@gmail.com>
2022-11-17 20:08:25 +0100

    >'

--- yatube/yatube/asgi.py
+++ yatube/yatube/asgi.py
@@ -4,16 +4,21 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
+import django
 
-from posts.routing import websocket_urlpatterns
+
+# from posts.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yatube.settings')
+django.setup()
+application = get_default_application()
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
-})
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             websocket_urlpatterns
+#         )
+#     ),
+# })
