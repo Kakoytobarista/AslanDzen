69dbbf504463cfb9a9e4467060233662735255f0
aslan <baristan1aslan@gmail.com>
2022-11-18 00:00:17 +0100

    Update cache image name

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -90,7 +90,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('192.241.153.145', 6379)],
+            "hosts": [('cache', 6379)],
         },
     },
 }
