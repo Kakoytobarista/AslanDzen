520c865db74032d2875bdb6316f8047ba9dea121
aslan <baristan1aslan@gmail.com>
2022-11-17 14:49:28 +0100

    Update consumer

--- yatube/posts/consumers.py
+++ yatube/posts/consumers.py
@@ -1,28 +1,28 @@
 import datetime
-import json
-from random import randint
-from time import sleep
 
 import json
 
 from asgiref.sync import sync_to_async
-from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-from django.shortcuts import get_object_or_404
-
 from posts.models import Post, Comment
+
+from django.contrib.auth.models import User
 
 
 class WSConsumers(AsyncWebsocketConsumer):
     author = None
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.post_id = None
+        self.post_group_name = None
+
     async def connect(self):
         self.post_id = self.scope['url_route']['kwargs']['post_id']
         self.post_group_name = 'posts_%s' % self.post_id
 
-        # Join room group
         await self.channel_layer.group_add(
             self.post_group_name,
             self.channel_name
@@ -31,7 +31,6 @@ class WSConsumers(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(
             self.post_group_name,
             self.channel_name
@@ -39,40 +38,41 @@ class WSConsumers(AsyncWebsocketConsumer):
 
     # Receive message from WebSocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        data = json.loads(text_data)
+        print(data)
+        message = data['message']
+        username = data['username']
+        room = data['room']
 
-        # Send message to room group
+        comment = await self.save_message(username, room, message)
+
         await self.channel_layer.group_send(
             self.post_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
-                'author1': "hello"
+                'username': username,
+                "created": datetime.datetime.strftime(comment.created, '%a, %d %B, %Y, %X')[:-3]
             }
         )
 
-    # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        username = event['username']
+        created = event['created']
 
         # Send message to WebSocket
-        await self.create_new_comment(message)
-        print("HEY")
         await self.send(text_data=json.dumps({
             'message': message,
-            'author': str(self.author),
-            'created': str(datetime.datetime.now().strftime("%a, %d %B, %Y, %H:%M"))
+            'username': username,
+            'created': created,
         }))
 
-    @database_sync_to_async
-    def create_new_comment(self, text):
-        post_id = self.scope['url_route']['kwargs']['post_id']
-        post = get_object_or_404(Post, id=post_id)
-        self.post_id = post_id
-        self.author = post.author
-        Comment.objects.create(
-            text=text,
-            post=post,
-            author=post.author
-        )
+    @sync_to_async
+    def save_message(self, username, room, message):
+        user = User.objects.get(username=username)
+        room = Post.objects.get(id=room)
+
+        comment = Comment.objects.create(author=user, post_id=room.id, text=message)
+        self.disconnect(close_code=200)
+        return comment
