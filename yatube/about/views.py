4bb0a1571887059f791c55de95d25bf4fc710851
aslan <baristan1aslan@gmail.com>
2022-11-18 04:03:21 +0100

    Update data to getting certificate, fix UI

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -1,11 +1,16 @@
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+
 from django.views.generic.base import TemplateView
 
 
+@method_decorator(cache_page(10 * 5), name='dispatch')
 class AboutAuthorView(TemplateView):
     template_name = 'about/author.html'
     title = 'About Author'
 
 
+@method_decorator(cache_page(10 * 5), name='dispatch')
 class AboutTechView(TemplateView):
     template_name = 'about/tech.html'
     title = 'Tech'
